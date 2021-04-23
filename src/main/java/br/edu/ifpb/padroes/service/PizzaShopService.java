47b7cb8cacba97eb31a466a86adc8310bae7e595
Adilson Junior <asc.juninho@hotmail.com>
2021-04-23 15:37:00 -0300

    Adiciona implementação do padrão 'Adapter'

--- src/main/java/br/edu/ifpb/padroes/service/PizzaShopService.java
+++ src/main/java/br/edu/ifpb/padroes/service/PizzaShopService.java
@@ -1,15 +1,16 @@
 package br.edu.ifpb.padroes.service;
 
 import br.edu.ifpb.padroes.api.damenos.DamenosPizza;
-import br.edu.ifpb.padroes.api.damenos.DamenosServiceImpl;
 import br.edu.ifpb.padroes.api.damenos.proxy.DamenosService;
 import br.edu.ifpb.padroes.api.damenos.proxy.DamenosServiceProxy;
 import br.edu.ifpb.padroes.api.pizzahot.PizzaHotPizza;
-import br.edu.ifpb.padroes.api.pizzahot.PizzaHotServiceImpl;
 import br.edu.ifpb.padroes.api.pizzahot.proxy.PizzaHotService;
 import br.edu.ifpb.padroes.api.pizzahot.proxy.PizzaHotServiceProxy;
 import br.edu.ifpb.padroes.domain.Pizza;
+import br.edu.ifpb.padroes.domain.adapter.DamenosAdapter;
+import br.edu.ifpb.padroes.domain.adapter.PizzahotAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PizzaShopService {
@@ -31,15 +32,17 @@ public class PizzaShopService {
 
     }
 
-    // TODO - implementar adapter para unificar pizzas vindas das APIs Damenos e PizzaHot num único método getPizzas()
-    // TODO - public List<Pizza> getPizzas() {}
+    public List<Pizza> getPizzas() {
+        List<Pizza> list = new ArrayList<>();
 
-    public List<DamenosPizza> getPizzasDamenos() {
-        return damenosService.getPizzas();
+        damenosService.getPizzas().forEach(damenosPizza -> {
+            list.add(new DamenosAdapter(damenosPizza));
+        });
+
+        pizzaHotService.getPizzas().forEach(pizzaHotPizza ->  {
+            list.add(new PizzahotAdapter(pizzaHotPizza));
+        });
+
+        return list;
     }
-
-    public List<PizzaHotPizza> getPizzasPizzaHot() {
-        return pizzaHotService.getPizzas();
-    }
-
 }
